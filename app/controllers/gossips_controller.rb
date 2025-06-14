4f0f992e43515fccbd1d90769890bf30c64f05a2
RosaBen <benchabane.rosa@outlook.com>
2025-06-14 03:47:29 +0200

    read gossips done

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -2,4 +2,14 @@ class GossipsController < ApplicationController
   def index
     @gossips = Gossip.order(created_at: :desc)
   end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  private
+
+  def gossip_params
+    params.require(:gossip).permit(:title, :content, :author_id) # Ajout author_id pour éviter warning
+  end
 end

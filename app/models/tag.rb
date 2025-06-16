53fe91fe925937f1c3d54df0faa40c8507877599
RosaBen <benchabane.rosa@outlook.com>
2025-06-16 19:18:22 +0200

    gossip with tags destroyed done

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :gossip_tags
+  has_many :gossip_tags, dependent: :destroy
   has_many :gossips, through: :gossip_tags
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
 end
